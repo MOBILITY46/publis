c29201534e4dd7e59b15cf6339d57c2fb3650fd9
David Jobe <daavid.jobe@gmail.com>
2021-03-11 16:12:41 +0100

    asset content type

--- npm/install.js
+++ npm/install.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const { download } = require('./binary')
 
-const asset = '/mobility46/publis/releases/download/v0.2.2/publis'
+const asset = '/mobility46/publis/releases/download/v0.2.0/publis'
 
 download(asset, path.resolve(__dirname, '..', 'bin', 'publis'))
